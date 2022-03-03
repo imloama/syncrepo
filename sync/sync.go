ceecb0fbc2e2c32e0046a56fc6d4a1b0680a229e
imloama <imloama@gmail.com>
2022-03-03 14:52:11 +0800

    feat: 优化

--- sync/sync.go
+++ sync/sync.go
@@ -12,20 +12,15 @@ func init()  {
 
 var repoSyncIds = make(map[string]cron.EntryID)
 
+
+
 func initSync(){
 	if len(config.Repos) == 0 {
 		return
 	}
 	crdata := cron.New(cron.WithSeconds())
 	for _, repo := range config.Repos {
-		id,err := crdata.AddFunc(repo.Cron, func() {
-			fmt.Printf("进行git仓库[%s]的同步\n", repo.Name)
-			err := GitService.Sync(repo.Name)
-			fmt.Printf("进行git仓库[%s]的同步结束\n", repo.Name)
-			if err!=nil{
-				fmt.Printf("进行git仓库[%s]的同步发生错误！%v\n", repo.Name, err)
-			}
-		})
+		id,err := crdata.AddJob(repo.Cron, NewSyncJob(repo))
 		repoSyncIds[repo.Name] = id
 		if err !=nil{
 			panic(err)
@@ -33,4 +28,24 @@ func initSync(){
 		}
 	}
 	crdata.Start()
+}
+
+
+type SyncJob struct{
+	Repo Repo
+}
+
+func NewSyncJob(repo Repo)SyncJob{
+	return SyncJob{
+		Repo: repo,
+	}
+}
+
+func (job SyncJob)Run(){
+	fmt.Printf("进行git仓库[%s]的同步\n", job.Repo.Name)
+	err := GitService.Sync(job.Repo.Name)
+	fmt.Printf("进行git仓库[%s]的同步结束\n", job.Repo.Name)
+	if err!=nil{
+		fmt.Printf("进行git仓库[%s]的同步发生错误！%v\n", job.Repo.Name, err)
+	}
 }
